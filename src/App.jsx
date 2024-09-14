c764ad3f42adfa917c2a91f9356c0e0402818a5d
sumithra <sumithra091004@gmail.com>
2024-09-15 00:59:50 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 
 const App = () => {
@@ -10,7 +12,9 @@ const App = () => {
     <>
     <div className='app'>
       <Navbar />
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
       <Footer />
     </div>  
     </>
