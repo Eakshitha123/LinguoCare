c44ea8a08d0b7f3c18f2d5ec22e36aa6aca53e9e
sumithra <sumithra091004@gmail.com>
2024-09-14 17:07:29 +0530

    Initial commit

---
+++ src/Components/Faq/Faq.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Faq.css'
+const Faq = () => {
+  return (
+    <>
+    
+    
+    </>
+
+  )
+}
+
+export default Faq
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+// /src/components/Header.js
+
+import React from 'react';
+//  import { Link } from 'react-router-dom';
+import './Navbar.css'; // Importing the CSS file
+
+const Header = () => {
+  return (
+    <header className="header">
+      
+      <nav>
+         <ul className="nav-links">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Login/Sign Up</li>
+        </ul> 
+
+       
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
