8106601ea6c1d887cce8c33390c455d826ebdccd
eakshitha <eakshitha2005@gmail.com>
2024-09-15 15:04:32 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';import Login from './Pages/Login/Login';
-
+import Login from './Pages/Login/Login';
 
 const App = () => {
   const [count, setCount] = useState(0);
--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -23,29 +23,4 @@ const Login = () => {
     );
 };
 
-export default Login;import React from 'react';
-import './Login.css';
-
-const Login = () => {
-    return (
-        <div className="login-container">
-            <h1>Login / Sign Up</h1>
-            <div className="login-options">
-                <div className="login-box">
-                    <h2>Login as User</h2>
-                    <button onClick={() => window.location.href = '/user-login'}>Login</button>
-                </div>
-                <div className="login-box">
-                    <h2>Login as Therapist</h2>
-                    <button onClick={() => window.location.href = '/therapist-login'}>Login</button>
-                </div>
-                <div className="login-box">
-                    <h2>Login as Supervisor</h2>
-                    <button onClick={() => window.location.href = '/supervisor-login'}>Login</button>
-                </div>
-            </div>
-        </div>
-    );
-};
-
 export default Login;
