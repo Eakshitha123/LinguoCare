a4bb0cee9e1ee8f34fa70f0c4c33c0e4e0d5250c
sumithra <sumithra091004@gmail.com>
2024-09-15 10:46:20 +0530

    Changes

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import  './Home.css'
 import Faq from '../../Components/Faq/Faq'
+import ContactUs from '../../Components/Contact/Contact'
 
 const Home = () => {
   return (
    <div>
     <Faq/>
+    <ContactUs/>
    </div>
   )
 }
