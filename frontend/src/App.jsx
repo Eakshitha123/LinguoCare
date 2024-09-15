7aa2e78cc589054a67912afededec7c31751f45c
mahek <mahekmodi04@gmail.com>
2024-09-15 14:50:59 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,21 @@ import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
-
+import Login from './Pages/Login/Login';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-      <Footer />
-    </div>  
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Login' element={<Login />} /> {/* Correct path */}
+        </Routes>
+        <Footer />
+      </div>
     </>
   );
 };
