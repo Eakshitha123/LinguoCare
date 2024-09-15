efaae1f38209da032678260617fac645b2b8867a
mahek <mahekmodi04@gmail.com>
2024-09-15 13:55:14 +0530

    Changes

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import Faq from '../../Components/Faq/Faq'
 import ContactUs from '../../Components/Contact/Contact'
 import Header from '../../Components/Header/Header'
 import Services from '../../Components/Services/Services'
+import About from '../../Components/About/About'
 
 const Home = () => {
   return (
    <div>
     <Header/>
     <Services/>
+    <About/>
     <Faq/>
     <ContactUs/>
    </div>
