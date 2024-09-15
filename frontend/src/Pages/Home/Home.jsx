d9365f17052c8d4467847c291644f90aed587d46
sumithra <sumithra091004@gmail.com>
2024-09-15 11:42:39 +0530

    Changes

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,14 +3,14 @@ import  './Home.css'
 import Faq from '../../Components/Faq/Faq'
 import ContactUs from '../../Components/Contact/Contact'
 import Header from '../../Components/Header/Header'
+import Services from '../../Components/Services/Services'
 
 const Home = () => {
   return (
    <div>
-    
     <Header/>
-    <Services/>
     <Faq/>
+    <Services/>
     <ContactUs/>
    </div>
   )
